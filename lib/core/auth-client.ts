5216e39d77791fa349e9e4d397fa5a6664e4c5c8
it-sainathgoud <whoppertechnologies@gmail.com>
2026-04-10 18:11:53 -0700

    Added bug reporting for endusers to report issues

--- lib/core/auth-client.ts
+++ lib/core/auth-client.ts
@@ -2,5 +2,8 @@ import { createAuthClient } from "better-auth/client"
 import { emailOTPClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
+  advanced: {
+    cookiePrefix: "mintax",
+  },
   plugins: [emailOTPClient()],
 })
